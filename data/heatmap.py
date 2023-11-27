e89b91cd13fff0a27c250121de76ee31cf08c79d
Vasily Postnicov <shamaz.mazum@gmail.com>
2023-11-27 15:02:29 +0300

    Fix images

--- data/heatmap.py
+++ data/heatmap.py
@@ -3,11 +3,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.load('shale-c3.npy')
+data = np.load('shale-s3.npy')
 
 plt.figure(figsize = (8, 8), dpi = 300)
 plt.rc('font', size = 20)
-plt.imshow(np.where(data < 0.1, data, 0.1), extent = (0, 160*2.25, 0, 160*2.25))
+plt.imshow(np.where(data < 0.1, data, 0.1), extent = (0, 160*2.25, 160*2.25, 0))
 plt.xlabel(r'$x, \mu m$')
 plt.ylabel(r'$y, \mu m$')
-plt.savefig('../images/shale-c3.png')
+plt.savefig('../images/shale-s3.png')
