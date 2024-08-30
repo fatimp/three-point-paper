e3f534e62c1f0ee95bf855f2e1a0198a9d3c97ad
Vasily Postnicov <shamaz.mazum@gmail.com>
2024-08-30 10:11:17 +0300

    Fix two images

--- timings/timings.py
+++ timings/timings.py
@@ -5,18 +5,18 @@ import matplotlib.pyplot as plt
 
 legend = ['$S_3$', '$C_3$', '$F_{sss}$', '$F_{ssv}$', '$F_{svv}$']
 names = map(lambda name: name + '.npy', ['s3', 'c3', 'surf3', 'surf2void', 'surfvoid2'])
-colors = ['b', 'r', 'g', 'k', 'm']
+colors = ['bo', 'rv', 'gP', 'k*', 'ms']
 
 plt.figure(figsize = (10, 9), dpi = 300)
-plt.rc('font', size = 25)
+plt.rc('font', size = 24)
 
 for (name, color) in zip(names, colors):
     data = np.load('gpu/' + name)
-    plt.plot(data[:,0]**3, data[:,1], color + '.-', linewidth = 3)
+    plt.plot(data[:,0]**3, data[:,1], color + '-', linewidth = 3, markersize = 10)
 
 plt.legend(legend)
 plt.yscale('log')
 plt.xscale('log')
 plt.xlabel('Number of voxels')
-plt.ylabel('Time of execution, s')
+plt.ylabel('Time of execution [s]')
 plt.savefig('timings-gpu.png')
