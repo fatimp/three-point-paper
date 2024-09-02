e73655413494048ac6bead3aab75cfbc3ca7848e
Vasily Postnicov <shamaz.mazum@gmail.com>
2024-09-02 14:17:24 +0300

    Fix 4 images

--- data/heatmap.py
+++ data/heatmap.py
@@ -2,13 +2,23 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import ticker
+import matplotlib as mpl
 
-data = np.load('shale-s3.npy')
+data = np.load('shale-avg.npy')
+
+def fuck(x, pos):
+    if pos%4 == 0:
+        f = ticker.LogFormatterSciNotation(10, False)
+        return f(x)
+    else:
+        return ''
 
 plt.figure(figsize = (8, 8), dpi = 300)
 plt.rc('font', size = 20)
 plt.imshow(data, extent = (0, 160/2*2.25, 160/2*2.25, 0), norm = 'log')
-plt.colorbar()
-plt.xlabel(r'$r_1 \cdot (1, 1, 0), \mu m$')
-plt.ylabel(r'$r_2 \cdot (-1, 1, 0), \mu m$')
-plt.savefig('../images/shale-s3.png', bbox_inches = 'tight')
+cb = plt.colorbar()
+cb.minorformatter = ticker.FuncFormatter(fuck)
+plt.xlabel(r'$r_1\ [\mu m]$')
+plt.ylabel(r'$r_2\ [\mu m]$')
+plt.savefig('../images/shale-avg.png', bbox_inches = 'tight')
